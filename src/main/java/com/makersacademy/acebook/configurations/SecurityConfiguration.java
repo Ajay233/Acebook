1146294c77167e4089f6975d1001131dec9dd831
Max Stevenson <maxstevenson@msn.com>
2019-01-16 14:38:06 +0000

    First commit: - Added spring security dependency to pom file. - Added spring secuirty test depedency to pom file. - Created Configurations package in src. - Created SecurityConfiguration file with hardcoded user credentials. - Created SecurityWebApplicationInitializer file.

---
+++ src/main/java/com/makersacademy/acebook/configurations/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package com.makersacademy.acebook.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//    // JDBC configuration:
+//    @Autowired
+//    DataSource dataSource;
+//    @Autowired
+//    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.jdbcAuthentication().dataSource(dataSource);
+//    }
+
+// In memory configuration:
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("John").password("test123").roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .logout();
+
+    }
+}
+
---
+++ src/main/java/com/makersacademy/acebook/configurations/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.makersacademy.acebook.configurations;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
