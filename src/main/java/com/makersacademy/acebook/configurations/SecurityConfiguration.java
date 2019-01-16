7815e1ff722ce09db66a26c12d8fad26683410e6
Max Stevenson <maxstevenson@msn.com>
2019-01-16 17:19:32 +0000

    Fourth commit: - Updated SecurityConfiguration to make use of DataSource - postgres db connection.

--- src/main/java/com/makersacademy/acebook/configurations/SecurityConfiguration.java
+++ src/main/java/com/makersacademy/acebook/configurations/SecurityConfiguration.java
@@ -1,31 +1,33 @@
 package com.makersacademy.acebook.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-//    // JDBC configuration:
-//    @Autowired
-//    DataSource dataSource;
-//    @Autowired
-//    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.jdbcAuthentication().dataSource(dataSource);
-//    }
+    // JDBC configuration:
+    @Autowired
+    DataSource dataSource;
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
 
 // In memory configuration:
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-
-        auth.inMemoryAuthentication()
-                .withUser(users.username("John").password("test123").roles("ADMIN"));
-    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        User.UserBuilder users = User.withDefaultPasswordEncoder();
+//
+//        auth.inMemoryAuthentication()
+//                .withUser(users.username("John").password("test123").roles("ADMIN"));
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
