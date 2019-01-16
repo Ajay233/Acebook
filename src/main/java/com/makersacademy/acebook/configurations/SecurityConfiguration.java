8a85f1736eb07057a4ae3e6c49388653ab5de322
Max Stevenson <maxstevenson@msn.com>
2019-01-16 15:11:07 +0000

    Second commit: - Remembered to actually put a logout button on index page for user to logout.

--- src/main/java/com/makersacademy/acebook/configurations/SecurityConfiguration.java
+++ src/main/java/com/makersacademy/acebook/configurations/SecurityConfiguration.java
@@ -31,6 +31,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest().authenticated()
+                //.antMatchers("/post").authenticated()
                 .and()
                 .formLogin()
                 .and()
