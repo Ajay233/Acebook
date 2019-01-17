da9827c42d56bab5f96ee97523083949a9d2b46d
SBriscoe2 <shabris@tiscali.co.uk>
2019-01-17 14:53:02 +0000

    fixed bug: Throwing error deleting posts with comments

--- src/main/resources/db/migration/V1.4__Description.sql
+++ src/main/resources/db/migration/V1.4__Description.sql
@@ -2,6 +2,6 @@ DROP TABLE IF EXISTS comments;
 
 CREATE TABLE comments (
   commentid bigserial PRIMARY KEY,
-  postid bigint REFERENCES posts(id),
+  postid bigint REFERENCES posts(id) ON DELETE CASCADE,
   comment varchar(200) NOT NULL
 )
